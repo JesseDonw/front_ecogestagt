741455878768fdcea8fccc136cfb4b249e0d7492
JesseDonw <168686587+JesseDonw@users.noreply.github.com>
2025-01-09 19:09:45 +0100

    erreur au niveau du signup

--- app/auth/signup.js
+++ app/auth/signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -7,7 +7,7 @@ import {
   StyleSheet,
   Alert,
   Image,
-  
+
 } from 'react-native';
 import { useRouter } from 'expo-router';
 import logo from '../../assets/logo.png';
@@ -17,152 +17,219 @@ import person from '../../assets/person.png';
 import mail from '../../assets/mail.png';
 import lock from '../../assets/lock.png';
 import { Colors } from '../../constants/Colors'
+import CustomError from '../../component/CustomError';
+import axios from 'axios';
 
 export default function SignUp() {
-    const router = useRouter();
-
-    const handleRegister = () => {
-        router.replace('/auth/check');
-    };
+  const router = useRouter();
 
   const [nom, setNom] = useState('');
   const [prenom, setPrenom] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [error, setError] = useState({
+    nom: false,
+    prenom: false,
+    email: false,
+    password: false,
+  });
 
   const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
   };
+  useEffect(() => {
+    if (nom) {
+      setError((e) => ({
+        ...e,
+        nom: false
+      }))
+    }
+  }, [nom])
 
-  const handleCreateAccount = () => {
-    if (!nom) {
-      Alert.alert('Erreur', 'Le champ Nom est requis.');
-      return;
+  useEffect(() => {
+    if (prenom) {
+      setError((e) => ({
+        ...e,
+        prenom: false
+      }))
+    }
+  }, [prenom])
+
+  useEffect(() => {
+    if (email) {
+      setError((e) => ({
+        ...e,
+        email: false
+      }))
+    }
+  }, [email])
+
+  useEffect(() => {
+    if (password) {
+      setError((e) => ({
+        ...e,
+        password: false
+      }))
+    }
+  }, [password])
+
+  const verifiedForm = () => {
+    const newErrors = {}
+
+    if (!nom?.trim()) {
+      newErrors.nom = true;
     }
 
-    if (!prenom) {
-      Alert.alert('Erreur', 'Le champ Prénom est requis.');
-      return;
+    if (!prenom?.trim()) {
+      newErrors.prenom = true;
+    }
+
+    if (email?.trim()) {
+      if (!validateEmail(email.trim())) {
+        newErrors.email = true;
+      }
+    } else {
+      newErrors.email = true;
+    }
+
+    if (!password?.trim()) {
+      newErrors.password = true;
     }
 
 
-    if (!email) {
-      Alert.alert('Erreur', "L'adresse email est requise.");
-      return;
-    }
+    setError(newErrors)
+    return Object.keys(newErrors).length === 0
+  }
 
-    if (!validateEmail(email)) {
-      Alert.alert('Erreur', "L'adresse email n'est pas valide.");
-      return;
-    }
+  const handleSignup = async () => {
+    try {
+      if (verifiedForm()) {
+        setLoading(true)
+        const data = {
+          nom: nom?.trim(),
+          prenom: prenom?.trim(),
+          email: email?.trim(),
+          password: password?.trim(),
+        }
 
-    if (!password) {
-      Alert.alert('Erreur', 'Le mot de passe est requis.');
-      return;
-    }
+        //const response = await axios.post(Key.Url + 'jwt/create/', data);
 
-    if (password.length < 6) {
-      Alert.alert(
-        'Erreur',
-        'Le mot de passe doit contenir au moins 6 caractères.'
-      );
-      return;
-    }
+        console.log('connecté :>> ');
+        router.replace('/auth/check')
+      }
 
-    // Simuler la création de compte
-    Alert.alert('Succès', 'Compte créé avec succès!');
+
+    } catch (error) {
+      console.log('error :>> ', error);
+    } finally {
+      setLoading(false)
+    }
   };
 
   return (
-    <KeyboardAwareScrollView contentContainerStyle={{ flexGrow:1}}>
-        <View style={styles.container}>
-      <Image
-        source={logo}
-        style={styles.logo}
-      />
-      
-        
-      {/* Champ Nom */}
-      <View style={[styles.inputContainer, styles.shadow]}>
+    <KeyboardAwareScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.container}>
         <Image
-          source={person} // Icône pour "Nom"
-          style={styles.person}
+          source={logo}
+          style={styles.logo}
         />
-        <TextInput
-          style={styles.input}
-          selectionColor={Colors.vert}
-          placeholder="Nom"
-          value={nom}
-          onChangeText={(text) => setNom(text)}
-        />
-      </View>
 
-      {/* Champ Prénom */}
-      <View style={[styles.inputContainer, styles.shadow]}>
-        <Image
-          source={person} // Icône pour "Prénom"
-          style={styles.person}
-        />
-        <TextInput
-    style={styles.input}
-    selectionColor={Colors.vert}
-    placeholder="Prénom"
-    value={prenom}
-    onChangeText={(text) => setPrenom(text)}
-        />
-      </View>
 
-      {/* Champ Email */}
-      <View style={[styles.inputContainer, styles.shadow]}>
-        <Image
-          source={mail} // Icône pour "Email"
-          style={styles.mail}
-        />
-        <TextInput
-          style={styles.input}
-          selectionColor={Colors.vert}
-          placeholder="Email"
-          keyboardType="email-address"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-      </View>
+        {/* Champ Nom */}
+        <View style={styles.viewInputError}>
+          <View style={[styles.inputContainer, styles.shadow]}>
+            <Image
+              source={person} // Icône pour "Nom"
+              style={styles.person}
+            />
+            <TextInput
+              style={styles.input}
+              selectionColor={Colors.vert}
+              placeholder="Nom"
+              value={nom}
+              onChangeText={(text) => setNom(text)}
+            />
+          </View>
+          {error.nom && <CustomError />}
+        </View>
 
-      {/* Champ Password */}
-      <View style={[styles.passwordContainer, styles.shadow]}>
-        <Image
-          source={lock} // Icône pour "Password"
-          style={styles.lock}
-        />
-        <TextInput
-          style={styles.passwordInput}
-          selectionColor={Colors.vert}
-          placeholder="Password"
-          secureTextEntry={!passwordVisible}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <TouchableOpacity
-          onPress={() => setPasswordVisible(!passwordVisible)}
-          style={styles.eyeIcon}
-        >
-          {passwordVisible ? (
-            <MaterialCommunityIcons name="eye-off" size={24} color="black" />
-          ) : (
-            <MaterialCommunityIcons name="eye" size={24} color="black" />
-          )}
+
+        {/* Champ Prénom */}
+        <View style={styles.viewInputError}>
+          <View style={[styles.inputContainer, styles.shadow]}>
+            <Image
+              source={person} // Icône pour "Prénom"
+              style={styles.person}
+            />
+            <TextInput
+              style={styles.input}
+              selectionColor={Colors.vert}
+              placeholder="Prénom"
+              value={prenom}
+              onChangeText={(text) => setPrenom(text)}
+            />
+          </View>
+          {error.prenom && <CustomError />}
+        </View>
+
+        {/* Champ Email */}
+        <View style={styles.viewInputError}>
+          <View style={[styles.inputContainer, styles.shadow]}>
+            <Image
+              source={mail} // Icône pour "Email"
+              style={styles.mail}
+            />
+            <TextInput
+              style={styles.input}
+              selectionColor={Colors.vert}
+              placeholder="Email"
+              keyboardType="email-address"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+          {error.email && <CustomError />}
+        </View>
+
+        {/* Champ Password */}
+        <View style={styles.viewInputError} >
+          <View style={[styles.passwordContainer, styles.shadow]}>
+            <Image
+              source={lock} // Icône pour "Password"
+              style={styles.lock}
+            />
+            <TextInput
+              style={styles.passwordInput}
+              selectionColor={Colors.vert}
+              placeholder="Password"
+              secureTextEntry={!passwordVisible}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+            <TouchableOpacity
+              onPress={() => setPasswordVisible(!passwordVisible)}
+              style={styles.eyeIcon}
+            >
+              {passwordVisible ? (
+                <MaterialCommunityIcons name="eye-off" size={24} color="black" />
+              ) : (
+                <MaterialCommunityIcons name="eye" size={24} color="black" />
+              )}
+            </TouchableOpacity>
+          </View>
+          {error.password && <CustomError />}
+        </View>
+
+        <TouchableOpacity style={styles.createButton} onPress={handleSignup}>
+          <Text style={styles.createButtonText}>Créer un compte</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push('/auth/signin')}>
+          <Text style={styles.linkText}> Avez vous déja un compte ? </Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.createButton} onPress={handleRegister}>
-        <Text style={styles.createButtonText}>Créer un compte</Text>
-      </TouchableOpacity>
-       <TouchableOpacity onPress={() => router.push('/auth/signin')}>
-                      <Text style={styles.linkText}> Avez vous déja un compte ? </Text>
-                  </TouchableOpacity>
-        </View>
-        
+
     </KeyboardAwareScrollView>
   );
 };
@@ -179,7 +246,7 @@ const styles = StyleSheet.create({
     width: 350,
     height: 180,
     marginBottom: 20,
-    
+
   },
 
   person: {
@@ -206,8 +273,8 @@ const styles = StyleSheet.create({
     height: 50,
     paddingHorizontal: 10,
     fontSize: 16,
-  
-   
+
+
   },
 
   passwordContainer: {
@@ -216,27 +283,26 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderWidth: 0.2,
     borderRadius: 15,
-    marginBottom: 15,
     width: '100%',
-    backgroundColor:'#ffffff',
+    backgroundColor: '#ffffff',
   },
-  inputContainer:{
+  inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     borderColor: '#ccc',
     borderWidth: 0.2,
     borderRadius: 15,
-    marginBottom: 15,
+
     width: '100%',
-    backgroundColor:'#ffffff',
+    backgroundColor: '#ffffff',
   },
   passwordInput: {
     fontFamily: "AbhayaLibreExtraBold",
     flex: 1,
     height: 50,
     paddingHorizontal: 10,
-    backgroundColor:'#ffffff',
-    fontSize:16,
+    backgroundColor: '#ffffff',
+    fontSize: 16,
 
   },
 
@@ -256,15 +322,15 @@ const styles = StyleSheet.create({
   },
   createButton: {
     backgroundColor: Colors.vert,
-        width: 'auto',
-        height: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 15,
-        paddingHorizontal: 25,
-        marginTop: 20,
+    width: 'auto',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    paddingHorizontal: 25,
+    marginTop: 20,
   },
-  
+
   createButtonText: {
     fontFamily: "AbhayaLibreExtraBold",
     color: '#fff',
@@ -276,6 +342,10 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
     marginTop: 30,
     fontSize: 14,
+  },
+viewInputError: {
+  marginBottom: 5,
+
 },
 });
- 
+
