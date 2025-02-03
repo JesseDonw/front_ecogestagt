f7ad8ee43c19c10c2bf9382d005e685f5cdd8187
Bruno-ADH <brunoadenhouessou@gmail.com>
2025-02-03 10:15:44 +0100

    pour la localisation avec open

---
+++ app/location/_layout.js
@@ -0,0 +1,44 @@
+import { useRouter, Link, Tabs, Stack } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+import Entypo from '@expo/vector-icons/Entypo';
+import Header from '../../component/Header';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Colors } from '../../constants/Colors';
+
+export default function MapLayout() {
+    const router = useRouter();
+    return(
+        <Stack>
+            <Stack.Screen 
+             name="maps"
+             options={{
+                headerTransparent: true,
+  headerTitle: "",
+  headerLeft: () => (
+    <Link href="/home" style={styles.wrapperBack}>
+      <Entypo
+        name="chevron-left"
+        size={26}
+        color={Colors.orange_foncer}
+        onPress={() => {
+          router.back();
+        }}
+      />
+    </Link>
+  ),
+  headerRight: () => <View style={{ pointerEvents: "none" }} />,
+             }}
+            />
+        </Stack>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    wrapperBack: {
+        backgroundColor: Colors.white,
+        padding: 8,
+        marginLeft: 2,
+        borderRadius: 14,
+    },
+})
